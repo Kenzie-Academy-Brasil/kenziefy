eb5b369f2e3b427213feac3cdcd858d76d856ae4
Hudson Carolino <hudson@kenzie.com.br>
2021-10-02 13:41:18 -0300

    add base music

---
+++ src/js/baseMusicas.js
@@ -0,0 +1,56 @@
+const baseMusicas = [
+    {
+        'name': 'Anonymous_Choir_-_Unus_Ex_Discipulis_Meis',
+        'artist': 'Anonymous',
+        'path': './src/musics/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14',
+        'artist': 'Anonymous',
+        'path': './src/musics/Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll',
+        'artist': 'Anonymous',
+        'path': './src/musics/James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'Anonymous_Choir_-_Unus_Ex_Discipulis_Meis',
+        'artist': 'Anonymous',
+        'path': './src/musics/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14',
+        'artist': 'Anonymous',
+        'path': './src/musics/Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll',
+        'artist': 'Anonymous',
+        'path': './src/musics/James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'Anonymous_Choir_-_Unus_Ex_Discipulis_Meis',
+        'artist': 'Anonymous',
+        'path': './src/musics/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14',
+        'artist': 'Anonymous',
+        'path': './src/musics/Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14.mp3',
+        'album': 'Good Enough',
+    },
+    {
+        'name': 'James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll',
+        'artist': 'Anonymous',
+        'path': './src/musics/James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll.mp3',
+        'album': 'Good Enough',
+    }
+];
